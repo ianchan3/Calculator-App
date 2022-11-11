2f418be39356507e8e101c8bbb1e1bc0c5542c63
ianchan3 <ianchan3138@gmail.com>
2022-11-11 15:09:50 -0800

    Square Root and Exponent Functionality Complete

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@ const keys = document.getElementsByTagName("li");
 const result = document.querySelector("p");
 const clear = document.querySelector(".clear");
 
+
 for (let i = 0; i < keys.length; i++) {
   if (keys[i].innerHTML === "=") {
     keys[i].addEventListener("click", calculate);
+  } else if (keys[i].innerHTML === "√") {
+    keys[i].addEventListener("click", squareRoot);
+  } else if (keys[i].innerHTML === "n²") {
+    keys[i].addEventListener("click", squared);
   } else {
     keys[i].addEventListener("click", addToCurrentValue(i))
   }
@@ -29,3 +34,11 @@ function calculate() {
 clear.addEventListener("click", () => {
   result.innerHTML = ""
 })
+
+function squareRoot() {
+  return (result.innerHTML = Math.sqrt(result.innerHTML));
+}
+
+function squared() {
+  return (result.innerHTML = Math.pow(result.innerHTML, 2));
+}
