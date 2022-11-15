e1bbdebce14980c7d75c3eb75deb84a3a24b55aa
ianchan3 <ianchan3138@gmail.com>
2022-11-15 13:07:51 -0800

    Plus Minus Functionality Complete

--- js/app.js
+++ js/app.js
@@ -23,6 +23,12 @@ for (let i = 0; i < keys.length; i++) {
         result.innerHTML += "*";   
       } else if (keys[i].innerHTML === "%") {
         result.innerHTML += "%";   
+      } else if (keys[i].innerHTML === "+/-") {
+        if (!result.innerHTML.includes("-")) {
+          result.innerHTML = "-" + result.innerHTML;   
+        } else if (result.innerHTML.includes("-")) {
+          result.innerHTML = result.innerHTML.slice(1);
+        }
       } else {
         result.innerHTML += keys[i].innerHTML;
       }
@@ -94,6 +100,8 @@ window.addEventListener("keydown", (e) => {
     clickOperation(e.key);
   } else if (e.key === "*") {
     clickOperation("x");
+  } else if (e.key === "±") {
+    clickOperation("+/-");
  
   } else if (e.key == "Enter" || e.key === "=") {
     clickEqual();
